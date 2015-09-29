242ce69b50649a1b5e129b0eb5b7294406533c6d
Malte Skoruppa <skoruppa@cs.uni-saarland.de>
2015-09-29 17:58:42 +0200

    added improved sql injection traversal

--- findvulns/findsqlinj_improved.groovy
+++ findvulns/findsqlinj_improved.groovy
@@ -1,20 +1,27 @@
 /*
-  Improved attempt to model possibly dangerous eval statements. Only
-  consider $_POST, $_GET, $_COOKIE and $_REQUEST sources.
+  First attempt to model possible SQL injections at a purely
+  syntactical level, when one of PHP's classic SQL *_query() functions
+  is used to perform the query.
  */
 
 attacker_sources = ["_GET", "_POST", "_COOKIE", "_REQUEST"]
 
+sql_query_funcs = ["mysql_query", "pg_query", "sqlite_query"]
+
 def warning( id, type, filename, lineno) {
-  "findeval_improved: In file " + filename + ": line " + lineno + " potentially dangerous (node id " + id + ", type " + type + ")"
+  "findsqlinj_improved: In file " + filename + ": line " + lineno + " potentially dangerous (node id " + id + ", type " + type + ")"
+}
+
+def unexpected( id, type, filename, lineno) {
+  "findsqlinj_improved: Unexpected first argument to SQL query call in " + filename + ", line " + lineno + " (node id " + id + ", type " + type + ")"
 }
 
 g.V()
-// find all eval expressions... or include, why not
-//.filter{ it.type == TYPE_INCLUDE_OR_EVAL && it.flags.contains(FLAG_EXEC_EVAL) }
-.filter{ it.type == TYPE_INCLUDE_OR_EVAL }
-// traverse to child
-.ithChildren(0)
+// find all call expressions where the called function is called "mysql_query" / "pg_query" / "sqlite_query"
+.filter{ sql_query_funcs.contains(it.code) && isCallExpression(it.nameToCall().next()) }.callexpressions()
+// traverse to arguments; note that we do not know whether to traverse
+// to the first or second argument, so just collect them all (1)
+.callToArguments()
 // match variables (argument could be a variable itself, or contain
 // variables within a string concatenation or an encapsulated list)
 .match{ it.type == TYPE_VAR }
--- findvulns/main.py
+++ findvulns/main.py
@@ -8,6 +8,7 @@ j.connectToDatabase()
 #command = file(os.path.join(os.path.dirname(__file__), "findeval.groovy")).read() + "\n"
 #command = file(os.path.join(os.path.dirname(__file__), "findinclude.groovy")).read() + "\n"
 command = file(os.path.join(os.path.dirname(__file__), "findeval_improved.groovy")).read() + "\n"
+#command = file(os.path.join(os.path.dirname(__file__), "findsqlinj_improved.groovy")).read() + "\n"
 
 res = j.runGremlinQuery( command)
 
